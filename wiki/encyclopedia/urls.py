d04ad502aae5e15a1622517b377bd060e78cacd4
Aaron <iaaron.xyz@outlook.com>
2022-12-18 13:49:51 -0600

    search entry working

--- wiki/encyclopedia/urls.py
+++ wiki/encyclopedia/urls.py
@@ -2,7 +2,9 @@ from django.urls import path
 
 from . import views
 
+app_name = "encyclopedia"
 urlpatterns = [
     path("", views.index, name="index"),
-    path("<str:title>", views.entry, name="article"),
+    path("wiki/<str:title>", views.entry, name="article"),
+    path("search", views.search, name="search"),
 ]
--- wiki/encyclopedia/views.py
+++ wiki/encyclopedia/views.py
@@ -14,3 +14,34 @@ def entry(request, title):
         "title": title,
         "entry_content": util.get_entry(title)
     })
+
+def search(request):
+    searched_entry = request.GET['q']
+    entries = util.list_entries()
+    num_entries = len(entries)
+    
+    similar_entries = []
+    # Check is searched entry match fully or partially wit the
+    # current entries
+    for i in range(num_entries):
+        # If match completely: return that entry
+        if searched_entry.lower() == entries[i].lower():
+            return entry(request, entries[i])
+        # else create a list of entries that partially match
+        elif searched_entry.lower() in entries[i].lower():
+            similar_entries += [entries[i]]
+    
+    # Print the list of those entries that partially match
+    if similar_entries:
+        return render(request, "encyclopedia/search.html", {
+            "similar_entries": similar_entries,
+            "num_entries": len(similar_entries),
+            "searched_entry": searched_entry,
+            "match": True
+        })
+    
+    # otherwise return search page  - not found
+    return render(request, "encyclopedia/search.html", {
+        "searched_entry": searched_entry,
+        "match": False
+    })
