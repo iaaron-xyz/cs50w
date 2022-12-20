5c513f363e6f70d39f688f90d78836684eedcbc5
Aaron <iaaron.xyz@outlook.com>
2022-12-20 03:56:09 -0600

    create auxiliar function to lowercase strings

--- wiki/encyclopedia/util.py
+++ wiki/encyclopedia/util.py
@@ -35,3 +35,11 @@ def get_entry(title):
         return f.read().decode("utf-8")
     except FileNotFoundError:
         return None
+
+
+def to_lower(string):
+    """
+    Convert to lowercase all the  alphabetic characters of a
+    string
+    """
+    return string.lower()
