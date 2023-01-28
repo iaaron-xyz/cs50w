8a18229b77f22b73cb1c6a5fce92b5d619aa8fa3
Aaron <iaaron.xyz@outlook.com>
2023-01-27 21:25:50 -0600

    create.html template basic layout

--- commerce/auctions/urls.py
+++ commerce/auctions/urls.py
@@ -4,6 +4,7 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
+    path("new_object", views.new_object, name="new_object"),
     path("login", views.login_view, name="login"),
     path("logout", views.logout_view, name="logout"),
     path("register", views.register, name="register")
